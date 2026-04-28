fd1b68cdec017312f3f8f749eb5a82dcb71bc7f3
kuklinski <mohammedmehdisaibat@gmail.com>
2026-04-28 10:24:28 +0100

    refactor: replace default dashboard with role-based views and DashboardController

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+
+        if ($user->role === 'admin') {
+            return view('admin.dashboard');
+        }
+
+        return view('guest.dashboard');
+    }
+}
---
+++ resources/views/guest/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome Traveler!</h1>
